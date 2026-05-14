fce7c462918db805a3fa626b744f27110367e20e
drebrahimhamdy30-web <drebrahimhamdy30@gmail.com>
2026-05-14 14:54:22 +0300

    Delete delivery/supabase-config.js

--- delivery/supabase-config.js
+++
@@ -1,5 +0,0 @@
-const SUPABASE_URL = 'https://rxtjoqulmgkkcohmgzgi.supabase.co';
-const SUPABASE_KEY = 'sb_publishable_O3icoIIQ5ptdzZ3UYLdaug_ktq63i0Q';
-
-const { createClient } = supabase;
-const db = createClient(SUPABASE_URL, SUPABASE_KEY);
