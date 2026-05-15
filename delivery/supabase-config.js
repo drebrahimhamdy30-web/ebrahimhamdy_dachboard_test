52913e5fbc037bf967db25195c43b0ccf1f5cdfa
drebrahimhamdy30-web <drebrahimhamdy30@gmail.com>
2026-05-15 19:26:56 +0300

    Add files via upload

---
+++ delivery/supabase-config.js
@@ -0,0 +1,5 @@
+const SUPABASE_URL = 'https://rxtjoqulmgkkcohmgzgi.supabase.co';
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ4dGpvcXVsbWdra2NvaG1nemdpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg3MDQ2OTUsImV4cCI6MjA5NDI4MDY5NX0.QVoJPtlRlRIz9tdhmdTZxHtKxrwAxJq0Je4QHkFKxj0';
+
+const { createClient } = supabase;
+const db = createClient(SUPABASE_URL, SUPABASE_KEY);
